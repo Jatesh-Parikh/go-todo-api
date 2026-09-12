70a29d86eb1f06078ee2ec8b14dc807b76c2cc9d
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-12 22:17:06 +0530

    add todo model

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Todo struct {
+	ID        int       `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	Completed bool      `json:"completed" db:"title"`
+	CreatedAt time.Time `json:"created_at" db:"title"`
+	UpdatedAt time.Time `json:"updated_at" db:"title"`
+	UserID    string    `json:"user_id" db:"title"`
+}
