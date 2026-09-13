ba7a6855e32b40f895286c473931319b9ca33490
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-13 17:31:55 +0530

    add delete todo

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -2,7 +2,7 @@ package repository
 
 import (
 	"context"
-	// "fmt"
+	"fmt"
 	"time"
 	"todo_api/internal/models"
 
@@ -147,4 +147,28 @@ func UpdateTodo(pool *pgxpool.Pool, id int, title string, completed bool, userID
 	}
 
 	return &todo, nil
+}
+
+func DeleteTodo(pool *pgxpool.Pool, id int, userID string) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+		DELETE FROM todos
+		WHERE id = $1 AND user_id = $2
+	`
+
+	var commandTag, err = pool.Exec(ctx, query, id, userID)
+
+	if err != nil {
+		return err
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("todo with id %d not found", id)
+	}
+
+	return nil
 }
