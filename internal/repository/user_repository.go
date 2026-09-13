886ab3bb9a9f1e38f2117fab01cb73abadedaf3f
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-13 13:06:42 +0530

    add get user by id

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -61,3 +61,31 @@ func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
 
 	return &user, nil
 }
+
+func GetUserByID(pool *pgxpool.Pool, id string) (*models.User, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+		SELECT id, email, password, created_at, updated_at
+		FROM users
+		WHERE id = $1
+	`
+	var user models.User
+
+	err := pool.QueryRow(ctx, query, id).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
