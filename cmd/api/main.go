22d0aafad09c744f22f3cd2d6f0d95c250a0fe32
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-13 11:25:27 +0530

    add main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"log"
+	"todo_api/internal/config"
+	"todo_api/internal/database"
+	"todo_api/internal/handlers"
+	"todo_api/internal/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	defer pool.Close()
+
+	var router *gin.Engine = gin.Default()
+	router.SetTrustedProxies(nil)
+
+	router.POST("/auth/register", handlers.CreateUserHandler(pool))
+	router.POST("/auth/login", handlers.LoginHandler(pool, cfg))
+
+	protected := router.Group("/todos")
+	protected.Use(middleware.AuthMiddleware(cfg))
+	{
+		protected.POST("", handlers.CreateTodoHandler(pool))
+		protected.GET("", handlers.GetAllTodosHandler(pool))
+		protected.GET("/:id", handlers.GetTodoByIDHandler(pool))
+		protected.PUT("/:id", handlers.UpdateTodoHandler(pool))
+		protected.DELETE("/:id", handlers.DeleteTodoHandler(pool))
+	}
+
+	// Middleware test route
+	router.GET("/protected-test", middleware.AuthMiddleware(cfg), handlers.TestProtectedHandler())
+
+	router.Run(":" + cfg.Port)
+}
