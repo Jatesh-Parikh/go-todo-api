3b622d02aa3d023253458c57ebe27aa36217a829
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-14 11:06:30 +0530

    add create todo handler

--- internal/handlers/todo_handler.go
+++ internal/handlers/todo_handler.go
@@ -1,1 +1,50 @@
 package handlers
+
+import (
+	"net/http"
+	// "strconv"
+	"todo_api/internal/repository"
+
+	"github.com/gin-gonic/gin"
+	// "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type CreateTodoInput struct {
+	Title     string `json:"title" binding:"required"`
+	Completed bool   `json:"completed"`
+}
+
+type UpdateTodoInput struct {
+	Title     *string `json:"title"`
+	Completed *bool   `json:"completed"`
+}
+
+func CreateTodoHandler(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userIDInterface, exists := c.Get("user_id")
+
+		if !exists {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user_id not found in context"})
+			return
+		}
+
+		userID := userIDInterface.(string)
+
+		var input CreateTodoInput
+
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		todo, err := repository.CreateTodo(pool, input.Title, input.Completed, userID)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, todo)
+	}
+}
